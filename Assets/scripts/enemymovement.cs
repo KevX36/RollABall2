26b52b7cc9b4458137de5666a4d6a900a1b71a4f
KevX36 <kevincard2007@gmail.com>
2025-09-29 14:35:04 -0300

    made a goal instead of an instant end

--- Assets/scripts/enemymovement.cs
+++ Assets/scripts/enemymovement.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemymovement : MonoBehaviour
 {
+    public Transform player;
+    private NavMeshAgent nav;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        nav = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            nav.SetDestination(player.position);
+        }
     }
 }
