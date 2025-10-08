cc8117edce749cddebd53f5388c7a8b0a4efe0a9
KevX36 <kevincard2007@gmail.com>
2025-10-08 10:45:27 -0300

    set up for level select

---
+++ Assets/scripts/LevelPick.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class LevelPick : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public gameObject Level1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
