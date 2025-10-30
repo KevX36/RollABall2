f91d427b0a5c09d465c753b91788257a6fb10c27
KevX36 <kevincard2007@gmail.com>
2025-10-30 17:12:35 -0300

    fixed main menu (the issue was something that would have worked with the old one but this is less clunky and can't be broken by screen size, should probably fix the buttons in the Control and item pages but that can wait)

---
+++ Assets/scripts/LevelPick2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelPick2 : MonoBehaviour
+{
+     public void LoadLevel1()
+     {
+        SceneManager.LoadScene("RollABall2-level-1");
+     }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("level2");
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("level3");
+    }
+    public void LoadControls()
+    {
+        SceneManager.LoadScene("controls");
+    }
+    public void LoadItems()
+    {
+        SceneManager.LoadScene("objectInfo");
+    }
+}
